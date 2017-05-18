0463f3bb631ef957f0d8aea49ccff2a7933b3009
kyuwankim <lpnids@gmail.com>
2017-05-18 22:33:50 +0900

    init

--- app/src/main/java/com/kyuwankim/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/helloandroid/MainActivity.java
@@ -4,30 +4,65 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tv ;
-    Button btn;
-
-    
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        tv.setVisibility(View.INVISIBLE);
-        btn = (Button) findViewById(R.id.button);
-        tv = (TextView) findViewById(R.id.textView);
+        init();
+        setClickListener();
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText("aaaaa");
-                tv.setVisibility(View.VISIBLE);
-            }
-        });
+    }
+
+    private void setClickListener() {
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+    }
+
+    private void init() {
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn1:
+                Toast.makeText(this, "Button 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn2:
+                Toast.makeText(this, "Button 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn3:
+                Toast.makeText(this, "Button 3", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn4:
+                Toast.makeText(this, "Button 4", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn5:
+                Toast.makeText(this, "Button 5", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
